282f2d4d1ab4912aba7c242317234f9213c797c6
Rey Balaba <reancirl@gmail.com>
2023-09-19 22:57:05 +0800

    Fix vite issue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,9 +13,12 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
-    <link rel="stylesheet" href="{{ asset('build/assets/app-3f5a6a01.css') }}">
-    <script type="module" src="{{ asset('build/assets/app-6e0eadfb.js') }}"></script>
+    @if (app()->environment('local'))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+        <link rel="stylesheet" href="{{ asset('build/assets/app-3f5a6a01.css') }}">
+        <script type="module" src="{{ asset('build/assets/app-6e0eadfb.js') }}"></script>
+    @endif
 
     <style>
         .dropdown {
